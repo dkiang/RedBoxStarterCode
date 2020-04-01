e458b210ab6ddf2025792d4da2c09ba85c6d1bfb
dkiang <dkiang@punahou.edu>
2020-04-01 10:11:35 -1000

    Create DVD.java

---
+++ DVD.java
@@ -0,0 +1,58 @@
+public class DVD
+{
+   //Declare the class variables.
+   
+   /** the title of the DVD */
+
+   /** the number of available copies */
+
+   
+   /** Constructs a DVD object and sets the number of copies
+    *  to 1.
+    *  @param t the title of the DVD
+    */
+    
+   public DVD(String t)
+   {
+   }
+   
+   /** Increments the number of available copies of this DVD.
+    */
+   public void incrementCopies()
+   {
+   }
+   
+   /** Decrements the number of available copies of this DVD.
+    */
+   public void decrementCopies()
+   {
+   }
+   
+   /** Gets the title of this DVD.
+    *
+    *  @return the title of the DVD.
+    */
+   public String getTitle()
+   {
+   }
+   
+   /** Gets the number of available copies for this DVD.
+    *
+    *  @return the number of available copies.
+    */
+   public int getNumCopies()
+   {
+   }
+   
+   /** Returns a representation of this DVD object as a
+    *  String in the format <title> copies: <num. copies>.
+    *
+    *  @return the String representation of the DVD.
+    */
+    
+   public String toString()
+   {
+      return;
+   }
+   
+}
